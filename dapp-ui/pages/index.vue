7141b51f5401a75214213286ae7d72f547eb89eb
ptsayli@gmail.com <ptsayli@gmail.com>
2019-07-30 16:05:55 +0530

    setup dapp folder structure

---
+++ dapp-ui/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        ETHIndia Workshop
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
